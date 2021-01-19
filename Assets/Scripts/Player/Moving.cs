b5dd0e07e011b0a6fd7dc8ea990d42d17b36f4b6
ParkYiJun <ydlwnsj25@gmail.com>
2021-01-20 01:19:50 +0900

    Player Moving ver.0
    Move To
    - Left&Right : transform.Translate(Vector2D*0.01f,Space.World)
    - Jump : Rigidbody2DComponent.AddForce(Vector2D.up * Physics2D.gravity*
      -1*10
    
    Key for
    - Left&Right : Arrow Keys
    - Jump : Space bar
    
    Consider
    - Left&Right : Change to Addforce Method
    - PlayerStartingPoint set : Just Remove or Add Codes for Game-Starting

---
+++ Assets/Scripts/Player/Moving.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    bool movable;
+    Rigidbody2D rigid;
+    private void Awake()
+    {
+        movable = true;
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        movable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movable)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                //rigid.AddForce(Vector2.left * rigid.mass * 100,ForceMode2D.Force);
+                transform.Translate(Vector2.left * 0.1f, Space.World);
+                Debug.Log("LeftArrow");
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //rigid.AddForce(Vector2.right * rigid.mass * 100, ForceMode2D.Force);
+                transform.Translate(Vector2.right * 0.1f, Space.World);
+                Debug.Log("RightArrow");
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigid.AddForce(Vector2.up * rigid.mass * Physics2D.gravity*-1*10, ForceMode2D.Force);
+                Debug.Log("Space");
+            }
+        }
+    }
+}

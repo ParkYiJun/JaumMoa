e1c10c9a958c53d2b9f06943cc2000d50b16f335
ParkYiJun <ydlwnsj25@gmail.com>
2021-01-21 22:16:53 +0900

    Fixed Size of Window to Play & Resize Player's Following Cam Rendering Rect
    CamRatio.cs
    - Set Camera's aspect : 1.0f;
    - Make new Rect for Dynamic window to keep its ratio
    - Add Application Quit : KEY Escape
    
    PlayerSetting
    1. Resoultion
    - Windowed Mode : Screen width - 500, Screen height - 500
    2. Standalone Player Options
    - Disable "Resizable Window"
    - Disable "Allow Fullscreen Switch"

---
+++ Assets/Scripts/Player/CamRatio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRatio : MonoBehaviour
+{
+    private void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+        cam.aspect = 1.0f;
+
+        float screenRatio = (float)Screen.height / Screen.width;
+        //float widthRatio = (screenRatio > 1.0f) ? 1.0f : screenRatio;
+
+        float x = (1 - screenRatio) / 2;
+
+        cam.rect = new Rect(x, 0, screenRatio, 1f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+            
+    }
+}

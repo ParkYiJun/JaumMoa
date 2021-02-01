67aa7818a36e945961199eef23edc1655331633d
ParkYiJun <ydlwnsj25@gmail.com>
2021-02-02 01:37:24 +0900

    Camera Move To Specific Position
    How to
    1. Direction Vector divide by 100
    2. That Vector use to Translate transform in Update()
    
    Etc.
    - Some *.cs files about Camera move to Scripts/Carmera Directory
    - Rename test scene to "BaseTesting"

---
+++ Assets/Scripts/Camera/CameraMoveTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveTest : MonoBehaviour
+{
+    public float x;
+
+    private void Start()
+    {
+        x = 0;   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            x += 0.1f;
+    }
+}
---
+++ Assets/Scripts/Camera/CameraMoving.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraMoving : MonoBehaviour
+{
+    float clipingX;
+    float clipingY;
+    Vector3 clipingVec;
+
+    float movingGap = 0.01f;
+
+    float vecX;
+    float vecY;
+    Vector3 vecToMove;
+    Vector3 unitVec;
+
+    private void Start()
+    {
+        clipingVec = new Vector3(5f, 1f, 0f);
+        vecToMove = new Vector3(0f, 0f, 0f);
+        VecInit();
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(vecToMove.magnitude) >= movingGap)
+        {
+            transform.Translate(unitVec);
+            adjustVecToMove();
+        }
+    }
+
+    private void VecInit()
+    {
+        adjustVecToMove();
+        unitVec = vecToMove / 100;
+    }
+
+    private void adjustVecToMove()
+    {
+        vecToMove = clipingVec - transform.position;
+        vecToMove.z = 0f;
+    }
+}
